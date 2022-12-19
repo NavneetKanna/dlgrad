3fc55ce637decf4a9c47c3d46bc2bb4d28bca7bb
NavneetKanna <navneetkanna04@gmail.com>
2022-12-19 21:39:16 +0000

    Fixed a small bug in drawing computational graph

--- dlgrad/graph.py
+++ dlgrad/graph.py
@@ -11,44 +11,21 @@ idx = 0
 
 # One of the input can be none such as in activation functions
 def add_nodes(op, output, input1, input2=None):
-    
     # Check if the output of the previous and input1/input2 shape is the same 
-    print("-------------------------------")
-    print("nodes")
-    print(nodes)
-    print(f"output {output}")
-    print(f"input1 {input1}")
-    print(f"input2 {input2}")
     if nodes:
         last_op = list(nodes.keys())[-1]
-        print(f"last_op {last_op}")
-        print(type(last_op))
         if op == 'Relu':
             nodes[f'{last_op} + Relu'] = nodes.pop(last_op)
             last_op = list(nodes.keys())[-1]
             return
-        print(f"inside nodes")
-        print("last item")
-        print(nodes[last_op][-1])
         if nodes[last_op][-1] in [input1, input2]:
-            print("last item matches")
             if input2:
-                print("if input2")
                 nodes[op] = [input1, input2, output]
-                print("nodes")
-                print(nodes)
             else:
-                print("if not input2")
                 nodes[op] = [input1, output]
-                print("nodes")
-                print(nodes)
     else:
-        print("if not nodes")
         if input2:
-            print("if input2")
             nodes[op] = [input1, input2, output]
-            print("nodes")
-            print(nodes)
         else:
             nodes[op] = [input1, output]
     
@@ -56,8 +33,6 @@ def add_nodes(op, output, input1, input2=None):
 def draw_cg():
     G = nx.DiGraph()
     for i in nodes:
-        print(f"i {i}")
-        print(f"nodes[i] {nodes[i]}")
         G.add_node(i)
         for j in nodes[i][:-1]:
             edges.append((j, i))
@@ -70,7 +45,6 @@ def draw_cg():
 
     G.add_edges_from(edges) 
     nx.drawing.nx_pydot.write_dot(G, r'/mnt/c/Users/navne/Documents/vs_code/dlgrad_main/dlgrad/net.dot')
-    # (graph,) = pydot.graph_from_dot_file(r'dlgrad/net.dot')
     (graph,) = pydot.graph_from_dot_file(r'/mnt/c/Users/navne/Documents/vs_code/dlgrad_main/dlgrad/net.dot')
     graph.write_png(r'/mnt/c/Users/navne/Documents/vs_code/dlgrad_main/dlgrad/net.png')
 
