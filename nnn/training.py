53cf197b50a217c54d9434036efb98fe2b2f4dfb
NavneetKanna <navneetkanna04@gmail.com>
2022-12-29 22:04:29 +0000

    changes

--- nnn/training.py
+++ nnn/training.py
@@ -3,46 +3,68 @@ from dlgrad.tensor import Tensor
 from dlgrad.graph import display_graph
 from dlgrad.afu import softmax
 import numpy as np
-from datasets.fetch_mnist import fetch_mnist
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from tqdm import trange
 
-lo_val = 0
 ff = True
-a = []
 loss_graph = []
 acc_graph = []
+pred = 0
+acc = 0
+loss = 0
 
+def train(model, train_loader, x_train, y_train, steps, BS, flag=False, lr=1e-3):
+    global ff, acc_graph, loss_graph, pred, glo_acc, glo_loss
 
-def train(model, x_train, y_train, flag, epoch, lr=1e-3):
-    global lo_val, ff, acc_graph, loss_graph
+    # for _ in (pbar := tqdm(range(steps))):
+    for _ in (pbar := trange(steps)):
+        x_batch_train, y_batch_train = train_loader.get_batch_data(x_train, y_train, BS)
+        
+        x = model.forward(x_batch_train, flag)
+        pred = x
+    
+        loss = crossentropy(x, y_batch_train, flag)
+        glo_loss = loss
+        
+        flag = False
+    
+        if ff: 
+            display_graph()
+            ff = False
 
-    x = model.forward(x_train, flag)
-    pred = x
-   
-    loss = crossentropy(x, y_train, flag)
-   
-    if ff: 
-        display_graph()
-        ff = False
-    lo_val += 1
+        Tensor.zero_grad()
 
-    loss.backward()
+        loss.backward()
 
-    for parameters in Tensor.get_parameters():
-        parameters.tensor = parameters.tensor - (lr*parameters.grad)
+        for parameters in Tensor.get_parameters():
+            parameters.tensor = parameters.tensor - (lr*parameters.grad)
 
-    if lo_val % 100 == 0:
-        acc = accuracy_score(y_train.tensor.T, np.argmax(softmax(pred), axis=1))
-        acc_graph.append(acc*100)
-        loss_graph.append(loss.tensor)
+        Tensor.save_for_backward.clear()
 
-    # Calculate accuracy and loss 
-    if lo_val == 1874:
-        acc = accuracy_score(y_train.tensor.T, np.argmax(softmax(pred), axis=1))
-        print("true")
-        print(y_train.tensor.T)
-        print("pred")
-        print(np.argmax(softmax(pred), axis=1))
-        print(f"acc {acc*100}%")
-        print(f"loss {loss.tensor}")
-        lo_val = 0
+        glo_acc = accuracy_score(y_batch_train.tensor.T, np.argmax(softmax(pred), axis=1))
+        pbar.set_postfix_str(f"loss {glo_loss.tensor} accuracy {glo_acc*100}%")
+
+    acc_graph.append(glo_acc)
+    loss_graph.append(glo_loss.tensor)
+        
+    # print("pred")
+    # print(np.argmax(softmax(pred), axis=1))
+
+def plot_metrics():
+    global loss_graph, acc_graph
+
+    fig, ax = plt.subplots()
+    ax.plot(loss_graph, label='Loss')
+    ax.plot(acc_graph, label='Accuracy')
+    ax.legend()
+    plt.close(fig)
+    fig.savefig("Metrics.png")
+
+def test(model, x_test, y_test):
+    x = model.forward(x_test)
+    loss = crossentropy(x, y_test)
+    print()
+    print(f"Test loss : {loss.tensor}")
+    acc = accuracy_score(y_test.tensor.T, np.argmax(softmax(x), axis=1))
+    print(f"Test accuracy : {acc*100}%")
