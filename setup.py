c3aa6452cb29fafe483fe71d715cc5815a8895c5
NavneetKanna <navneetkanna04@gmail.com>
2023-01-04 07:37:08 +0000

    hello

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-setup(name='dlgrad',  packages=find_packages())
+# setup(name='dlgrad',  packages=find_packages())
 # setup(name='examples', version='1.0', packages=find_packages())
