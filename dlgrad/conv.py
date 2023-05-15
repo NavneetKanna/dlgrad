0d472a66a222fe5410738daa2471cdb886131c05
NavneetKanna <navneetkanna04@gmail.com>
2023-05-15 16:30:10 +0100

    im2col
    Former-commit-id: ad0421eebb3d85c0c9488afb141bfa690c381177

--- dlgrad/conv.py
+++ dlgrad/conv.py
@@ -11,32 +11,39 @@ class Conv2D:
         self.stride = stride
         self.padding = padding
 
-        self.filters = np.random.random((self.out_channels, self.in_channels, self.kernal_size, self.kernal_size))
+        # (16, 3, 4, 4)
+        self.kernel= np.random.random((self.out_channels, self.in_channels, self.kernal_size, self.kernal_size))
+        # (16, 1, 48)
+        self.im2col_kernel = np.random.randn(self.out_channels, 1, (self.kernal_size**2)*self.in_channels)
 
     def __call__(self, data): 
-        self.conv(data)
+        self.new_w = ((data.shape[-2] - self.kernal_size + 2*0)//self.stride) + 1 
+        self.new_h = ((data.shape[-1] - self.kernal_size + 2*0)//self.stride) + 1 
+        self.conv_out = np.zeros((data.shape[0], self.out_channels, self.new_w, self.new_h), dtype=np.float32)
+        return self.conv(data)
 
-    def im2col(self, x):
-        self.new_w = ((x.shape[2] - self.kernal_size + 2*0)//self.stride) + 1 
-        self.new_h = ((x.shape[1] - self.kernal_size + 2*0)//self.stride) + 1 
-
-        res = np.zeros((self.kernal_size*self.kernal_size*x.shape[0], self.new_w*self.new_h))
+    def im2col(self, data):
+        res = np.zeros((self.kernal_size*self.kernal_size*data.shape[0], self.new_w*self.new_h))
 
         idx = 0
-        for row in range(0, x.shape[-2]-self.kernal_size+ 1, self.stride):
-            for col in range(0, x.shape[-1]-self.kernal_size + 1, self.stride):
-                patch = x[..., row:row+self.kernal_size, col:col+self.kernal_size].reshape(-1)
+        for row in range(0, data.shape[-2]-self.kernal_size+ 1, self.stride):
+            for col in range(0, data.shape[-1]-self.kernal_size + 1, self.stride):
+                patch = data[..., row:row+self.kernal_size, col:col+self.kernal_size].reshape(-1)
                 res[:, idx] = patch
                 idx += 1
         return res.T
 
     def conv(self, data):
-        res = self.im2col(data[0, ...])
-        final = res @ self.filters.T
-        fi = final.reshape(self.new_w, self.new_h).round(4)
-        # for channel in self.in_channels:
-        #     res = self.im2col(data[channel, ...])
-        #     final = res @ self.filters.T
-        #     fi = final.reshape(self.new_w, self.new_h).round(4)
+        for bs in range(data.shape[0]):
+            for no_of_filter in range(self.out_channels):
+                res = self.im2col(data[bs, ...])
+                res = res @ self.im2col_kernel[no_of_filter].T
+                res = res.reshape(self.new_w, self.new_h).round(4) # col2img
+                self.conv_out[bs, no_of_filter, ...] = res
+            
+        return self.conv_out 
 
-        return fi
+
+class MaxPool2D:
+    def __init__(self) -> None:
+        pass
