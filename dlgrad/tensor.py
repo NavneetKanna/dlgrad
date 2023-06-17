c5bf42ebe5ff1a775f617866c7cc34d5165215ad
NavneetKanna <navneetkanna04@gmail.com>
2023-06-17 22:19:26 +0100

    changes
    Former-commit-id: 89a282edf4e68d01161ccebac2570e00734f550e

--- dlgrad/tensor.py
+++ dlgrad/tensor.py
@@ -63,28 +63,28 @@ class Tensor:
 
         return output 
 
-    def ReLU(self, matrix: Tensor) -> Tensor:
-        backward_list.append(matrix)
-        output = Tensor(np.maximum(0, matrix.tensor), 'Relu')
+    def ReLU(self) -> Tensor:
+        backward_list.append(self)
+        output = Tensor(np.maximum(0, self.tensor), 'Relu')
 
         # if not CG.stop_processing: CG.add_nodes('ReLU', output.tensor, matrix.tensor)
 
         def backward():
-            matrix.tensor[matrix.tensor <= 0] = 0
-            matrix.tensor[matrix.tensor > 0] = 1
-            matrix.grad = (matrix.tensor * output.grad)
+            self.tensor[self.tensor <= 0] = 0
+            self.tensor[self.tensor > 0] = 1
+            self.grad = (self.tensor * output.grad)
 
         output._backward = backward
 
         return output
 
-    def softmax(self, matrix: Tensor) -> np.ndarray:
+    def softmax(self) -> np.ndarray:
         """
         We are subtracting each row with the maximum element, a kind of normalization,
         because the exp can get huge.
         """
-        max_of_row = np.amax(matrix.tensor, axis=1, keepdims=True)
-        matrix_exp = np.exp(matrix.tensor - max_of_row)
+        max_of_row = np.amax(self.tensor, axis=1, keepdims=True)
+        matrix_exp = np.exp(self.tensor - max_of_row)
         matrix_sum = np.sum(matrix_exp, axis=1, keepdims=True)
         result = matrix_exp / matrix_sum
 
