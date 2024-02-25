b5a0128d67b359ca218ebaf7f39c1d7141d9bed9
NavneetKanna <navneetkanna04@gmail.com>
2024-02-25 16:35:59 +0530

    now list also does not get c buffer
    Former-commit-id: b5ca3ee73562b55b05d17b4ede5d84c8cc15fbec

--- dlgrad/buffer.py
+++ dlgrad/buffer.py
@@ -4,7 +4,7 @@ from dlgrad.helpers import get_list_dim
 
 class Buffer:
     """
-    C buffer is not created for scalar data and for arrays/list of dim 1
+    C buffer is not created for scalar data and n dim list 
     
     """
 
@@ -12,11 +12,7 @@ class Buffer:
         self._data = data
     
     @staticmethod
-    def create_scalar_buffer(data: Union[int, float]):
-        return Buffer(data) 
+    def create_scalar_buffer(data: Union[int, float]): return Buffer(data) 
     
     @staticmethod
-    def create_list_buffer(data: list):
-        dim = get_list_dim(data)
-        if dim == 1: return Buffer(data)
-        else: pass
+    def create_list_buffer(data: list): return Buffer(data)
