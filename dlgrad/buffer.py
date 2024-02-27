ef1920e2d5103d45878bdd72032697fc1c459e4b
NavneetKanna <navneetkanna04@gmail.com>
2024-02-27 21:17:49 +0530

    added lot of checks of rand

--- dlgrad/buffer.py
+++ dlgrad/buffer.py
@@ -1,6 +1,14 @@
 from typing import Union
-import ctypes
-from dlgrad.helpers import get_list_dim
+import ctypes as ct
+# from dlgrad.helpers import get_list_dim
+
+
+dll = ct.CDLL('../backend/c/test.so')
+dll.create_rand_buffer.argtypes = ct.POINTER(ct.c_int), ct.c_int
+# dll.func.restype = ct.POINTER(ct.c_char)
+# dll.freeMem.argtypes = ct.c_void_p,
+# dll.freeMem.restype = None
+
 
 class Buffer:
     """
@@ -16,3 +24,9 @@ class Buffer:
     
     @staticmethod
     def create_list_buffer(data: list): return Buffer(data)
+
+    #TODO: Restrict the dim to 4, so things become easier
+    @staticmethod
+    def create_rand_buffer(shape): 
+
+        dll.create_rand_buffer()
