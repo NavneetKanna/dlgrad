f9f2a7b26e6e40b2f00d47c50478f1eaea702939
NavneetKanna <navneetkanna04@gmail.com>
2024-02-27 21:37:30 +0530

    yay rand is working, well till correctly passing the args to c
    Former-commit-id: 10616970638f93e20b67afbf4480515a7e08ccd3

--- backend/c/buffer.c
+++ backend/c/buffer.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 
 void create_rand_buffer(int bs, int ch, int w, int h) {
+    printf("bs %d, ch %d, w %d, h %d\n", bs, ch, w, h);
 
 }
+
+
+/*
+clang -shared -o test.so buffer.c 
+*/
