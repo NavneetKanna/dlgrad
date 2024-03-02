fd3405429009196243d7d3ae24fed15fb677dff8
NavneetKanna <navneetkanna04@gmail.com>
2024-03-02 20:38:01 +0530

    atexit was what i was looking for :)
    Former-commit-id: 074e4cf8e122552d7a5699a8068d7a40b44c6444

--- backend/c/buffer.c
+++ backend/c/buffer.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-float *create_rand_buffer() {
-    
+float *create_rand_buffer(int len) {
+    float *data = malloc(len * sizeof(float));
+
+    return data;
 }
 
+void free_buf(void *data) {
+    printf("freeing data\n");
+    free(data);
+}
 
 /*
 clang -shared -o test.so buffer.c 
