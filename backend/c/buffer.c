4411b7b3b77bbc7573b9d968d64394aed1138d0c
NavneetKanna <navneetkanna04@gmail.com>
2024-03-03 16:37:44 +0530

    nice everything is working as of now :)
    Former-commit-id: dc6ee1bb45723bdcf38d21910ce08f9589e93de3

--- backend/c/buffer.c
+++ backend/c/buffer.c
@@ -4,7 +4,10 @@
 
 float *create_rand_buffer(int len) {
     float *data = malloc(len * sizeof(float));
-
+    srand(time(NULL));
+    for (int i=0; i<len; i++) {
+        data[i] = (float)rand() / (float)RAND_MAX;
+    }
     return data;
 }
 
