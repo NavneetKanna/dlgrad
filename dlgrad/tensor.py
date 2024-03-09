daac23a42ded008cdd9875d725c759caf6488942
NavneetKanna <navneetkanna04@gmail.com>
2024-03-09 15:45:14 +0530

    gosh indexing is a hard problem, still working on it, i feel like i am getting somewhere
    Former-commit-id: f695d9068db2db6d898e3187ad1b113dee1e54c9

--- dlgrad/tensor.py
+++ dlgrad/tensor.py
@@ -6,7 +6,7 @@
 from __future__ import annotations
 from typing import Union
 from dlgrad.c_interface import c_rand_buffer
-from dlgrad.helpers import ShapeError
+from dlgrad.helpers import ShapeError, calculate_stride
 import ctypes
 import atexit
 import numpy as np
@@ -14,7 +14,7 @@ import numpy as np
 class Tensor:
     # __slots__ = "grad"
 
-    def __init__(self, data: Union[list, int, float], _len: int=None, _shape: tuple = None):
+    def __init__(self, data: Union[list, int, float], _total_bytes: int=None, _shape: tuple = None):
         # self.device = ...
         # self.stride = ...
         # self.grad = ...   
@@ -25,16 +25,20 @@ class Tensor:
         if isinstance(data, list): 
             self._data = data
             self.len = len(data)
+        # TODO: An enum for this ?
         if isinstance(data, ctypes.POINTER(ctypes.c_float)): 
             self._data = data
-            self._len = _len
+            self._len = _total_bytes
             self._shape = _shape
+            self._strides = calculate_stride(_shape)
+            self.ndim = len(_shape)
 
         atexit.register(self.cleanup)
 
     # ***** DCOPS (data creation ops) *****
     @staticmethod
     def rand(*shape) -> Tensor: 
+        # TODO: ((((?))))
         if isinstance(shape[0], tuple): shape = shape[0]
         if isinstance(shape[0], list): shape = tuple(*shape)
 
@@ -44,20 +48,49 @@ class Tensor:
         size = 1
         for i in shape: size *= i
         
-        return Tensor(c_rand_buffer._create(size), size, shape)
+        return Tensor(c_rand_buffer._create(size), _total_bytes=size, _shape=shape)
     
     def numpy(self):
         ptr = ctypes.cast(self._data, ctypes.POINTER(ctypes.c_float * self._len))
         data = np.frombuffer(ptr.contents, dtype=np.float32).reshape(self._shape)
         print(data)
 
-    def __repr__(self) -> str:
-        return f"{self._data}"
+    # def __repr__(self) -> str:
+    #     return f"{self._data}"
 
     def cleanup(self): 
         if isinstance(self._data, ctypes.POINTER(ctypes.c_float)): c_rand_buffer._free(self._data)  
     
-    # TODO: Have index func 
+    def __getitem__(self, indices): 
+        # TODO: all int
+        """
+        start_idx = data(idx[dim]*stride[dim] till stride[dim])
+        
+        """
+        # TODO: slices
+        print(type(self))
+        print(indices)
+        print(type(indices))
+
+        if self.ndim == 2:
+            row = ...
+            col = ...
+            if type(indices) == int:
+                row = indices
+                if row > self.ndim: raise IndexError 
+                offset = row * self._strides[0] + col
+                
+            elif type(indices) == tuple:
+                pass
+
+            offset = row * self._shape[1] + col
+
+      
+
+
+
+        
+
 
 """
 https://stackoverflow.com/questions/7343833/srand-why-call-it-only-once
