270e462b8defa163602388ef1c44985039408a31
NavneetKanna <navneetkanna04@gmail.com>
2024-03-10 10:10:53 +0530

    a better way of printing numpy

--- dlgrad/tensor.py
+++ dlgrad/tensor.py
@@ -14,7 +14,7 @@ import numpy as np
 class Tensor:
     # __slots__ = "grad"
 
-    def __init__(self, data: Union[list, int, float], view: bool = False, _offset=0, _view_len: int = None, _len: int = None, _shape: tuple = None):
+    def __init__(self, data: Union[list, int, float], view: bool = False, _offset=0, _len: int = None, _shape: tuple = None):
         # self.device = ...
         # self.stride = ...
         # self.grad = ...   
@@ -30,7 +30,6 @@ class Tensor:
             self._data = data
             self._offset = _offset
             self._len = _len
-            self._view_len = _view_len
             # TODO: Add enum, here 4 = 4 bytes for float32 
             self._total_bytes = _len * 4
             self._shape = _shape
@@ -56,11 +55,22 @@ class Tensor:
         return Tensor(c_rand_buffer._create(size), _len = size, _shape=shape)
     
     def numpy(self):
+        """
+        sd = ctypes.addressof(a._data.contents) + 3 * ctypes.sizeof(ctypes.c_float)
+        ptr = (ctypes.c_float * 3).from_address(sd)
+        np.frombuffer(sd, count=3, dtype=np.float32)
+        """
         # TODO: Why cast if type is already ctypes pointer ?
-        ptr = ctypes.cast(self._data, ctypes.POINTER(ctypes.c_float * self._len))
-        # TODO: Change 4 to dtype
-        data = np.frombuffer(ptr.contents, offset=self._offset*4, count=self._view_len if self._view_len else self._len, dtype=np.float32).reshape(self._shape)
+        sd = ctypes.addressof(self._data.contents) + self._offset * ctypes.sizeof(ctypes.c_float)
+        ptr = (ctypes.c_float * self._len).from_address(sd)
+        data = np.frombuffer(ptr, count=self._len, dtype=np.float32).reshape(self._shape)
         print(data)
+
+
+        # ptr = ctypes.cast(self._data, ctypes.POINTER(ctypes.c_float * self._len))
+        # # TODO: Change 4 to dtype
+        # data = np.frombuffer(ptr.contents, offset=self._offset*4, count=self._view_len if self._view_len else self._len, dtype=np.float32).reshape(self._shape)
+        # print(data)
 
     # TODO: If its a view add a bool = true
     # def __repr__(self) -> str:
@@ -79,7 +89,7 @@ class Tensor:
                 offset = row * self._strides[0] 
                 # if offset != 0: offset -= 1
                 size = self._strides[0]
-                return Tensor(self._data, view=True, _offset=offset, _view_len = size, _len = self._len, _shape=(size,))
+                return Tensor(self._data, view=True, _offset=offset, _len = size, _shape=(size,))
                 
             elif type(indices) == tuple:
                 row, col = indices
