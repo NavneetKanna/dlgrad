61efaa11987abab03b5c2f7a9b4599b86ea96960
NavneetKanna <navneetkanna04@gmail.com>
2024-03-25 19:25:15 +0530

    added dtype check for scalar
    Former-commit-id: 1077fc6593326ec52e2b0296d57a80327e52cce0

--- dlgrad/dtype.py
+++ dlgrad/dtype.py
@@ -3,3 +3,8 @@ from typing import Final
 class dtypes:
     float32: Final
     float32_ptr: Final
+    int32: Final
+
+    @staticmethod
+    def from_py(data):
+        return dtypes.float32 if isinstance(data, float) else dtypes.int32
