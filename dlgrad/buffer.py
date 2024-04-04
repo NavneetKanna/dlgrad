f767b9ee6cd0cab81db353a31cfbb8999e35015b
NavneetKanna <navneetkanna04@gmail.com>
2024-04-04 07:31:33 +0530

    Buffer class working (tested for rand, dynamically compiling)

--- dlgrad/buffer.py
+++ dlgrad/buffer.py
@@ -8,14 +8,25 @@ import tempfile
 import ctypes
 
 class Buffer:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, data) -> None:
+        self.data_buffer = data
 
-    def create_random_buffer(self, length: int):
-        prg = C._random_buffer(length)
+    @staticmethod
+    def create_random_buffer(length: int):
+        prg = C._random_buffer()
         with tempfile.NamedTemporaryFile(delete=True) as output_file:
             subprocess.check_output(args=['clang', '-x', 'c', '-', '-shared', '-o', str(output_file.name)], input=prg.encode('utf-8'))
             rand_dll = ctypes.CDLL(str(output_file.name))
             rand_dll.create_rand_buffer.argtypes = (ctypes.c_int,)
             rand_dll.create_rand_buffer.restype = ctypes.POINTER(ctypes.c_float) 
+            return Buffer(rand_dll.create_rand_buffer(length))
 
+    @staticmethod
+    def free(data):
+        prg = C._free()
+        with tempfile.NamedTemporaryFile(delete=True) as output_file:
+            subprocess.check_output(args=['clang', '-x', 'c', '-', '-shared', '-o', str(output_file.name)], input=prg.encode('utf-8'))
+            free_dll = ctypes.CDLL(str(output_file.name))
+            free_dll.free_buf.argtypes = ctypes.c_void_p,
+            free_dll.free_buf.restype = None
+            free_dll.free_buf(data) 
