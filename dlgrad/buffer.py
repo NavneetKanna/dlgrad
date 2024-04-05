0e9e831c6ded9ef131f2d7783489e83a580f2be2
NavneetKanna <navneetkanna04@gmail.com>
2024-04-05 22:04:00 +0530

    made create rand faster

--- dlgrad/buffer.py
+++ dlgrad/buffer.py
@@ -6,36 +6,48 @@ from dlgrad.c_code import C
 import subprocess
 import tempfile
 import ctypes
+import time
+import os
+from dlgrad.helpers import get_temp_loc
 
 
-# TODO: cdll is taking the most time
 class Buffer:
-    _rand_dll = None
+    rand_temp_file = False
 
     def __init__(self, data) -> None:
         self.data_buffer = data
 
-
     @staticmethod
     def create_random_buffer(length: int):
         prg = C._random_buffer()
-        with tempfile.NamedTemporaryFile(delete=True) as output_file:
-            subprocess.check_output(args=['clang', '-o2', '-march=native', '-fPIC', '-x', 'c', '-', '-shared', '-o', str(output_file.name)], input=prg.encode('utf-8'))
+        # ctypes.CDLL was taking the most time when i was compiling the prg everytime this func was called
+        # and also there is no need to compile everytime this func was called, hence compiling only once
+        # and reading the shared file, and now ctypes.CDLL is fast and is no longer taking time.
+        for f in os.listdir(get_temp_loc()):
+            print(f)
+            if f.startswith("rand_buffer"):
+                s = time.perf_counter()
+                rand_dll = ctypes.CDLL(f"{get_temp_loc()}/{f}")
+                e = time.perf_counter()
+                print(f"time to open cdll {e-s:.4f}s")
+            else:
+                with tempfile.NamedTemporaryFile(delete=False, dir=get_temp_loc(), prefix="rand_buffer") as output_file:
+                    print(str(output_file.name))
+                    subprocess.check_output(args=['clang', '-o2', '-march=native', '-fPIC', '-x', 'c', '-', '-shared', '-o', str(output_file.name)], input=prg.encode('utf-8'))
+                    s = time.perf_counter()
+                    rand_dll = ctypes.CDLL(str(output_file.name))
+                    e = time.perf_counter()
+                    print(f"time to open cdll {e-s:.4f}s")
 
-            print(Buffer._rand_dll)
-            
-            rand_dll = ctypes.CDLL(str(output_file.name))
-            Buffer._rand_dll = rand_dll
             rand_dll.create_rand_buffer.argtypes = (ctypes.c_int,)
             rand_dll.create_rand_buffer.restype = ctypes.POINTER(ctypes.c_float) 
-
             return Buffer(rand_dll.create_rand_buffer(length))
 
     @staticmethod
     def free(data):
         prg = C._free()
         with tempfile.NamedTemporaryFile(delete=True) as output_file:
-            subprocess.check_output(args=['clang', '-x', 'c', '-', '-shared', '-o', str(output_file.name)], input=prg.encode('utf-8'))
+            subprocess.check_output(args=['clang', '-march=native', '-fPIC', '-x', 'c', '-', '-shared', '-o', str(output_file.name)], input=prg.encode('utf-8'))
             free_dll = ctypes.CDLL(str(output_file.name))
             free_dll.free_buf.argtypes = ctypes.c_void_p,
             free_dll.free_buf.restype = None
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import random 
 import ctypes
 import numpy as np
 import time
@@ -6,36 +5,41 @@ import torch
 from tinygrad import Tensor
 import dlgrad
 
-shape = (100, 100)
+shape = (10000, 10000)
 
 
-def to_numpy(fa, l, s):
-    sd = ctypes.addressof(fa) + 0 * ctypes.sizeof(ctypes.c_float)
-    ptr = (ctypes.c_float * l).from_address(sd)
-    data = np.frombuffer(ptr, count=l, dtype=np.float32).reshape(s)
-    print(data.shape)
+
+# s = time.perf_counter()
+# a = ctypes.CDLL("rand_buffer.so")
+# e = time.perf_counter()
+# print(f"time {e-s:.4f}s")
+# def to_numpy(fa, l, s):
+#     sd = ctypes.addressof(fa) + 0 * ctypes.sizeof(ctypes.c_float)
+#     ptr = (ctypes.c_float * l).from_address(sd)
+#     data = np.frombuffer(ptr, count=l, dtype=np.float32).reshape(s)
+#     print(data.shape)
 
 def dl():
     s = time.perf_counter()
-    a = dlgrad.Tensor.rand(shape)
+    _ = dlgrad.Tensor.rand(shape)
     e = time.perf_counter()
     print(f"dl {e-s:.4f}s")
 
 def num():
     s = time.perf_counter()
-    a = np.random.randn(*shape)
+    _ = np.random.randn(*shape)
     e = time.perf_counter()
     print(f"numpy {e-s:.4f}s")
 
 def to():
     s = time.perf_counter()
-    a = torch.rand(shape)
+    _ = torch.rand(shape)
     e = time.perf_counter()
     print(f"torch {e-s:.4f}s")
 
 def ti():
     s = time.perf_counter()
-    a = Tensor.rand(shape).numpy()
+    _ = Tensor.rand(shape).numpy()
     e = time.perf_counter()
     print(f"tinygrad {e-s:.4f}s")
 
