b2f2f56e59b200d85e56700d698f282bc8afa2aa
NavneetKanna <navneetkanna04@gmail.com>
2024-04-07 12:40:54 +0530

    now indexing is working with new buffer class, starting out with dispatcher
    Former-commit-id: 43a98bd29105d638df79af400804d6c8d1aa7fe2

---
+++ dlgrad/dispatch.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from dlgrad.tensor import Tensor
+
+class Dispatcher:
+    def __init__(self) -> None:
+        pass
+
+    def dispatch(x: Tensor, y: Tensor):
+        pass
