b108a45ca0da945b77cdab2ca933eee657eee823
NavneetKanna <navneetkanna04@gmail.com>
2024-06-15 21:37:25 +0530

    linear and broadcasting working, but still lot to do :)

--- dlgrad/runtime/cpu.py
+++ dlgrad/runtime/cpu.py
@@ -8,7 +8,7 @@ from dlgrad.dtype import dtypes
 import subprocess
 import ctypes
 import tempfile
-from dlgrad.helpers import get_temp_loc, check_temp_file_exists
+from dlgrad.helpers import get_temp_loc, check_temp_file_exists, BroadcastHelper
 from dlgrad.buffer import Buffer
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -26,15 +26,16 @@ class CPU:
         if temp_file:
             add_dll = ctypes.CDLL(f"{get_temp_loc()}/{temp_file}")
         else:
-            prg = C._add(c_dtype, out_len=x._len) 
+            # TODO: Check out_len > 0
+            prg = C._add(c_dtype, out_len=BroadcastHelper.out_len) 
             with tempfile.NamedTemporaryFile(delete=False, dir=get_temp_loc(), prefix=name) as output_file: 
                 temp_file = str(output_file.name)
                 subprocess.check_output(args=['clang', '-O2', '-march=native', '-fPIC', '-x', 'c', '-', '-shared', '-o', temp_file], input=prg.encode('utf-8'))
                 add_dll = ctypes.CDLL(temp_file)
 
-        add_dll.add.argtypes = [ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float)]
-        add_dll.add.restype = ctypes.POINTER(ctypes.c_float) 
-        data = add_dll.add(x._data, y._data)
+        add_dll.add_with_broadcasting.argtypes = [ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.c_int, ctypes.c_int]
+        add_dll.add_with_broadcasting.restype = ctypes.POINTER(ctypes.c_float) 
+        data = add_dll.add_with_broadcasting(x._data, y._data, x._len, y._len)
         if data is None:
             # TODO: create a new error
             print("Error: could not allocate memory")
