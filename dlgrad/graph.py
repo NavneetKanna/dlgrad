42e3fb3a9c37d83fa6ea14d91cbd0487836a6142
NavneetKanna <navneetkanna04@gmail.com>
2024-07-28 08:11:23 +0530

    added graph class

--- dlgrad/graph.py
+++ dlgrad/graph.py
@@ -1,14 +1,21 @@
 import networkx as nx
-from dlgrad.helpers import get_graph
+from dlgrad.helpers import get_graph, BinaryOps, UnaryOps, BufferOps
 
 
-if get_graph():
-    g = nx.DiGraph()
+class Graph:
+    def __init__(self):
+        self.G = nx.DiGraph() if get_graph() else None
+        self.ops_colour = {}
 
+    def add_node(self, label: str):
+        if self.G is not None:
+            self.G.add_node(label)
 
+    def add_edge(self, source: str, target: str):
+        if self.G is not None:
+            self.G.add_edge(source, target)
 
+graph_instance = Graph()
 
-
-    
-
-
+add_node = graph_instance.add_node
+add_edge = graph_instance.add_edge
