00df9638a53e33d28a24fcef7074873a50eb3a1a
NavneetKanna <navneetkanna04@gmail.com>
2024-08-02 20:13:55 +0530

    fixed a very annoying bug, will refactor later
    Former-commit-id: 1388ead12ba168c6c76aaa7546059087c47721bc

--- dlgrad/buffer.py
+++ dlgrad/buffer.py
@@ -28,20 +28,29 @@ class Buffer:
 
     @staticmethod
     def free(data) -> None:
-        temp_file = check_temp_file_exists(starts_with="free") 
-        if temp_file:
-            temp_file = f"{get_temp_loc()}/{temp_file}"
-            free_dll = ctypes.CDLL(temp_file)
-        else:
-            prg = C._free()
-            with tempfile.NamedTemporaryFile(delete=False, dir=get_temp_loc(), prefix="free") as output_file:
-                temp_file = str(output_file.name)
-                subprocess.check_output(args=['clang', '-o2', '-march=native', '-fPIC', '-x', 'c', '-', '-shared', '-o', temp_file], input=prg.encode('utf-8'))
-                free_dll = ctypes.CDLL(temp_file)
+        # print("in buffer free")
+        # print(data)
+        libc = ctypes.CDLL(None)  # Load the standard C library
+
+        if data is not None:
+            # print("data is not none")
+            libc.free(data)
+            data = None
+
+        # temp_file = check_temp_file_exists(starts_with="free") 
+        # if temp_file:
+        #     temp_file = f"{get_temp_loc()}/{temp_file}"
+        #     free_dll = ctypes.CDLL(temp_file)
+        # else:
+        #     prg = C._free()
+        #     with tempfile.NamedTemporaryFile(delete=False, dir=get_temp_loc(), prefix="free") as output_file:
+        #         temp_file = str(output_file.name)
+        #         subprocess.check_output(args=['clang', '-o2', '-march=native', '-fPIC', '-x', 'c', '-', '-shared', '-o', temp_file], input=prg.encode('utf-8'))
+        #         free_dll = ctypes.CDLL(temp_file)
    
-        free_dll.free_buf.argtypes = ctypes.c_void_p,
-        free_dll.free_buf.restype = None
-        free_dll.free_buf(data) 
+        # free_dll.free_buf.argtypes = ctypes.c_void_p,
+        # free_dll.free_buf.restype = None
+        # free_dll.free_buf(data) 
         
-        if os.path.exists(temp_file):
-            os.remove(temp_file) 
+        # if os.path.exists(temp_file):
+        #     os.remove(temp_file) 
