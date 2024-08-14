bf91f7462e87ae486732d1fa2d0e5a1f7a8413d5
NavneetKanna <navneetkanna04@gmail.com>
2024-08-14 07:56:12 +0530

    fixed bug in cpu
    Former-commit-id: 2fc7cb84a5092892c0141881101019beac161be6

--- dlgrad/runtime/cpu.py
+++ dlgrad/runtime/cpu.py
@@ -16,7 +16,7 @@ if TYPE_CHECKING:
     from dlgrad.tensor import Tensor
 
 class CPUHelper:
-    dlls: defaultdict[ctypes.CDLL]
+    dlls = defaultdict(ctypes.CDLL)
     
 # TODO: is it dll or sha_lib ?
 class CPU:
@@ -28,12 +28,7 @@ class CPU:
             return x.data + y.data
 
         c_dtype = dtypes.get_c_dtype(dtype)
-        name = ""
-        add_dll = None
-
-        add_dll = None
-        data = None
-        prg = None
+        temp_file = ''
 
         if axis == 0:
             prg = C.add_axis0(c_dtype, out_len=BroadcastHelper.out_len)
@@ -85,13 +80,9 @@ class CPU:
             ]
             add_dll.add_with_broadcasting.restype = ctypes.POINTER(ctypes.c_float)
             # TODO: assuming y is getting broadcasted, maybe pass from dispatch ?
-            import time
-            s = time.perf_counter()
             data = add_dll.add_with_broadcasting(
                 x.data.buffer, y.data.buffer, x.numel, y.numel, x.shape[1]
             )
-            e = time.perf_counter()
-            print(f"actual {e-s:.4f}s")
         elif axis == 1:
             add_dll.add_with_broadcasting.argtypes = [
                 ctypes.POINTER(ctypes.c_float),
@@ -100,24 +91,16 @@ class CPU:
                 ctypes.c_int,
             ]
             add_dll.add_with_broadcasting.restype = ctypes.POINTER(ctypes.c_float)
-            import time 
-            s = time.perf_counter()
             data = add_dll.add_with_broadcasting(
                 x.data.buffer, y.data.buffer, x.numel, y.numel
             )
-            e = time.perf_counter()
-            print(f"actual {e-s:.4f}s")
         elif axis == -1:
             add_dll.add.argtypes = [
                 ctypes.POINTER(ctypes.c_float),
                 ctypes.POINTER(ctypes.c_float),
             ]
             add_dll.add.restype = ctypes.POINTER(ctypes.c_float)
-            import time
-            s = time.perf_counter()
             data = add_dll.add(x.data.buffer, y.data.buffer)
-            e = time.perf_counter()
-            print(f"actual {e-s:.4f}s")
 
         if data is None:
             # TODO: create a new error
@@ -131,11 +114,7 @@ class CPU:
         #     return x.data + y.data
 
         c_dtype = dtypes.get_c_dtype(dtype)
-
-        sum_dll = None
-        data = None
-        prg = None
-        name = ""
+        temp_file = ''
 
         if axis == 0:
             prg = C.sum_axis0(c_dtype)
@@ -196,11 +175,7 @@ class CPU:
         elif axis == -1:
             sum_dll.sum.argtypes = [ctypes.POINTER(ctypes.c_float), ctypes.c_int]
             sum_dll.sum.restype = ctypes.POINTER(ctypes.c_int)
-            import time
-            s = time.perf_counter()
             data = sum_dll.sum(x.data.buffer, x.numel)
-            e = time.perf_counter()
-            print(f"actual: {e-s:.4f}s")
 
         if data is None:
             # TODO: create a new error
@@ -220,56 +195,58 @@ class CPU:
     def matmul(x: Tensor, y: Tensor, dtype: dtypes) -> Buffer:
         if not isinstance(x.data, Buffer):
             pass
-        else:
-            c_dtype = dtypes.get_c_dtype(dtype)
-            name = get_shared_lib_name("matmul", c_dtype, x.device.name)
-            matmul_dll = CPUHelper.dlls.get(name)
+            
+        temp_file = ''
+        c_dtype = dtypes.get_c_dtype(dtype)
+        name = get_shared_lib_name("matmul", c_dtype, x.device.name)
+        matmul_dll = CPUHelper.dlls.get(name)
 
-            if not matmul_dll:
-                prg = C.matmul(c_dtype)
-                with tempfile.NamedTemporaryFile(
-                    delete=False, dir=get_temp_loc(), prefix=name
-                ) as output_file:
-                    temp_file = str(output_file.name)
-                    subprocess.check_output(
-                        args=[
-                            "clang",
-                            "-O2",
-                            "-march=native",
-                            "-x",
-                            "c",
-                            "-",
-                            "-shared",
-                            "-o",
-                            temp_file,
-                        ],
-                        input=prg.encode("utf-8"),
-                    )
-                    matmul_dll = ctypes.CDLL(temp_file, mode=os.RTLD_LAZY)
-                    CPUHelper.dlls[name] = matmul_dll
+        if not matmul_dll:
+            prg = C.matmul(c_dtype)
+            with tempfile.NamedTemporaryFile(
+                delete=False, dir=get_temp_loc(), prefix=name
+            ) as output_file:
+                temp_file = str(output_file.name)
+                subprocess.check_output(
+                    args=[
+                        "clang",
+                        "-O2",
+                        "-march=native",
+                        "-x",
+                        "c",
+                        "-",
+                        "-shared",
+                        "-o",
+                        temp_file,
+                    ],
+                    input=prg.encode("utf-8"),
+                )
+                matmul_dll = ctypes.CDLL(temp_file, mode=os.RTLD_LAZY)
+                CPUHelper.dlls[name] = matmul_dll
 
-            matmul_dll.matmul.argtypes = [
-                ctypes.POINTER(ctypes.c_float),
-                ctypes.POINTER(ctypes.c_float),
-                ctypes.c_int,
-                ctypes.c_int,
-                ctypes.c_int,
-            ]
-            matmul_dll.matmul.restype = ctypes.POINTER(ctypes.c_float)
-            data = matmul_dll.matmul(
-                x.data.buffer, y.data.buffer, x.shape[0], x.shape[1], y.shape[1]
-            )
-            if data is None:
-                # TODO: create a new error
-                print("Error: could not allocate memory")
+        matmul_dll.matmul.argtypes = [
+            ctypes.POINTER(ctypes.c_float),
+            ctypes.POINTER(ctypes.c_float),
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+        ]
+        matmul_dll.matmul.restype = ctypes.POINTER(ctypes.c_float)
+        data = matmul_dll.matmul(
+            x.data.buffer, y.data.buffer, x.shape[0], x.shape[1], y.shape[1]
+        )
+        if data is None:
+            # TODO: create a new error
+            print("Error: could not allocate memory")
 
-            return Buffer(data, temp_file)
+        return Buffer(data, temp_file)
 
     @staticmethod
     def transpose(x: Tensor, dtype: dtypes):
         if not isinstance(x.data, Buffer):
             pass
 
+        temp_file = ''
         c_dtype = dtypes.get_c_dtype(dtype)
         name = get_shared_lib_name("transpose", c_dtype, x.device.name)
         transpose_dll = CPUHelper.dlls.get(name) 
@@ -314,6 +291,7 @@ class CPU:
     def uniform(length: int, low=0.0, high=1.0) -> Buffer:
         name = get_shared_lib_name("uniform")
         rand_dll = CPUHelper.dlls.get(name) 
+        temp_file = ''
 
         if not rand_dll:
             prg = C.random_buffer()
@@ -357,6 +335,7 @@ class CPU:
     def ones(length: int) -> Buffer:
         name = get_shared_lib_name("transpose")
         ones_dll = CPUHelper.dlls.get(name)
+        temp_file = ''
 
         if not ones_dll:
             prg = C.ones_buffer()
