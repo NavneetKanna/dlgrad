08d3c5bdd46d6c1b909e9875e0eafcacc6fbd0a1
NavneetKanna <navneetkanna04@gmail.com>
2024-08-14 21:48:59 +0530

    moving onto activation functions

--- dlgrad/nn/__init__.py
+++ dlgrad/nn/__init__.py
@@ -10,5 +10,8 @@ class Linear:
         bound = 1 / math.sqrt(inp_dim)
         self.bias = Tensor.rand(out_dim, low=-bound, high=bound)
 
+    # x@weight.T + bias
     def __call__(self, x: Tensor) -> Tensor:
+        assert x.shape[-1] == self.weight.shape[-1], f"{x.shape} and {self.weight.shape} does not match"
+
         return x.linear(self.weight, self.bias)
