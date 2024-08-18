f6cb43119992f7ecd5bca5560cd9b8f32feb4e37
NavneetKanna <navneetkanna04@gmail.com>
2024-08-18 19:41:59 +0530

    Added setup in test nn
    Former-commit-id: 1a5842f13aca6678826efa2b34e8daa53a71c5c6

--- test/test_nn.py
+++ test/test_nn.py
@@ -12,37 +12,35 @@ from dlgrad.tensor import Tensor
 
 
 class TestNN(unittest.TestCase):
-    def test_linear(self):
+    def setUp(self):
         BS, inp_dim, out_dim = 4, 3, 5 
-        x = Tensor.rand((BS, inp_dim))
-        fc = nn.Linear(inp_dim, out_dim)
-        dl_out = fc(x)
+        self.x = Tensor.rand((BS, inp_dim))
+        self.fc = nn.Linear(inp_dim, out_dim)
+        self.to_fc = torch.nn.Linear(inp_dim, out_dim)
 
-        w = fc.weight
-        b = fc.bias
+    def test_linear(self):
+        dl_out = self.fc(self.x)
+
+        w = self.fc.weight
+        b = self.fc.bias
         with torch.no_grad():
-            x = torch.tensor(x.numpy(), dtype=torch.float32, device="cpu")
-            fc = torch.nn.Linear(inp_dim, out_dim)
-            fc.weight[:] = torch.tensor(w.numpy(), dtype=torch.float32, device="cpu")
-            fc.bias[:] = torch.tensor(b.numpy(), dtype=torch.float32, device="cpu")
-            tr_out = fc(x)
+            x = torch.tensor(self.x.numpy(), dtype=torch.float32, device="cpu")
+            self.to_fc.weight[:] = torch.tensor(w.numpy(), dtype=torch.float32, device="cpu")
+            self.to_fc.bias[:] = torch.tensor(b.numpy(), dtype=torch.float32, device="cpu")
+            tr_out = self.to_fc(x)
 
         np.testing.assert_allclose(dl_out.numpy(), tr_out.detach().numpy(), atol=1e-4) 
 
     def test_linear_with_relu(self):
-        BS, inp_dim, out_dim = 4, 3, 5 
-        x = Tensor.rand((BS, inp_dim))
-        fc = nn.Linear(inp_dim, out_dim)
-        dl_out = Tensor.relu(fc(x))
+        dl_out = Tensor.relu(self.fc(self.x))
 
-        w = fc.weight
-        b = fc.bias
+        w = self.fc.weight
+        b = self.fc.bias
         with torch.no_grad():
-            x = torch.tensor(x.numpy(), dtype=torch.float32, device="cpu")
-            fc = torch.nn.Linear(inp_dim, out_dim)
-            fc.weight[:] = torch.tensor(w.numpy(), dtype=torch.float32, device="cpu")
-            fc.bias[:] = torch.tensor(b.numpy(), dtype=torch.float32, device="cpu")
-            tr_out = torch.relu(fc(x))
+            x = torch.tensor(self.x.numpy(), dtype=torch.float32, device="cpu")
+            self.to_fc.weight[:] = torch.tensor(w.numpy(), dtype=torch.float32, device="cpu")
+            self.to_fc.bias[:] = torch.tensor(b.numpy(), dtype=torch.float32, device="cpu")
+            tr_out = torch.relu(self.to_fc(x))
 
         np.testing.assert_allclose(dl_out.numpy(), tr_out.detach().numpy(), atol=1e-4) 
 
