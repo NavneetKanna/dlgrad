465e48d6cc7e4d78b6fdff0f915d538e299d34ec
NavneetKanna <navneetkanna04@gmail.com>
2024-10-20 15:56:50 +0530

    adding docstrings

--- test.py
+++ test.py
@@ -9,16 +9,17 @@ e = time.perf_counter()
 print(f"{e-s}s")
 
 
-import torch
+# import torch
 
-s = time.perf_counter()
-a = torch.tensor(1, device="cpu")
-e = time.perf_counter()
-print(f"{e-s:f}s")
 
-from tinygrad import Tensor
+# s = time.perf_counter()
+# a = torch.tensor(1, device="cpu")
+# e = time.perf_counter()
+# print(f"{e-s:f}s")
 
-s = time.perf_counter()
-a = Tensor(1, device="clang")
-e = time.perf_counter()
-print(f"{e-s:f}s")
+# from tinygrad import Tensor
+
+# s = time.perf_counter()
+# a = Tensor(1, device="clang")
+# e = time.perf_counter()
+# print(f"{e-s:f}s")
