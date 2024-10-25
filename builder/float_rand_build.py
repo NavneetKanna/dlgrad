412ad759b8301b4dd9e4b269f1ba9a38ddd88ace
NavneetKanna <navneetkanna04@gmail.com>
2024-10-25 10:06:09 +0530

    added .h file
    Former-commit-id: 5ba7ccc1cce0f29044f90b5e3fbdd9c47e1e4ae5

--- builder/float_rand_build.py
+++ builder/float_rand_build.py
@@ -3,10 +3,10 @@ from cffi import FFI
 ffi = FFI()
 ffi.cdef("float *uni(int numel); void free_uni(float* ptr);")
 ffi.set_source("_uni", """
-    #include "src/c/float_rand.h"
-    #include "src/c/pcg_basic.h"
+    #include "dlgrad/src/c/float_rand.h"
+    #include "dlgrad/src/c/pcg_basic.h"
 """, 
-sources=['src/c/float_rand.c', "src/c/pcg_basic.c"],
+sources=['dlgrad/src/c/float_rand.c', "dlgrad/src/c/pcg_basic.c"],
 libraries=["m"],
 extra_compile_args=["-O3"])
 
---
+++ dlgrad/src/c/float_rand.h
@@ -0,0 +1,7 @@
+#ifndef UNI_H
+#define UNI_H
+
+float *uni(int numel);
+void free_uni(float* ptr);
+
+#endif
