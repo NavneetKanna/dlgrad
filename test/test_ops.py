ea2ef5b5d5be5843550a9d1c8705a202989de0f1
NavneetKanna <navneetkanna04@gmail.com>
2024-11-03 21:46:38 +0530

    add test same shape working
    Former-commit-id: 0bb06c52553de7fc09955ccc0841f6fc485345c1

--- test/test_ops.py
+++ test/test_ops.py
@@ -4,21 +4,27 @@ import torch
 import numpy as np
 
 
+# TODO: Not an efficient way, need dlgrad to support creating Tensor from np, see tinygrad
+def run(shapes: list[tuple], func):
+    dla = Tensor.rand(shapes[0])
+    dlb = Tensor.rand(shapes[1])
+    toa = torch.tensor(dla.numpy())
+    tob = torch.tensor(dlb.numpy())
+
+    np.testing.assert_allclose(func(dla, dlb).numpy(), func(toa, tob).numpy(), atol=1e-6, rtol=1e-3)
+    
+
 class TestOps(unittest.TestCase):
-    def test_add(self):
+    def test_add_same_shape(self):
         sh1 = (2, 3)
         sh2 = (2, 3)
-        da = Tensor.rand(sh1)
-        db = Tensor.rand(sh2)
-
-        dc = da+db
-
-        ta = torch.tensor(da.numpy())
-        tb = torch.tensor(db.numpy())
-
-        tc = ta+tb
-
-        np.testing.assert_allclose(tc.numpy(), dc.numpy(), atol=1e-6, rtol=1e-3)
+        run([sh1, sh2], lambda x, y: x+y)
+        sh1 = (100, 100)
+        sh2 = (100, 100)
+        run([sh1, sh2], lambda x, y: x+y)
+        sh1 = (78, 91)
+        sh2 = (78, 91)
+        run([sh1, sh2], lambda x, y: x+y)
 
 
 if __name__ == "__main__":
