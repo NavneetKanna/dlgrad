48836ed5a03aa252d81f953b0f203ea09a7e8779
NavneetKanna <navneetkanna04@gmail.com>
2024-11-08 16:20:12 +0530

    updated rand builder

--- dlgrad/builder/float_rand_build.py
+++ dlgrad/builder/float_rand_build.py
@@ -6,7 +6,7 @@ root_dir = os.path.dirname(os.path.abspath(__file__ + "/.."))
 
 ffi = FFI()
 
-ffi.cdef("float *uniform(int numel); void free_uniform(float* ptr);")
+ffi.cdef("float *uniform(int numel, float low, float high); void free_uniform(float* ptr);")
 ffi.set_source("_uniform", f"""
     #include "{root_dir}/src/c/float_rand.h"
     #include "{root_dir}/src/c/pcg_basic.h"
--- dlgrad/nn/__init__.py
+++ dlgrad/nn/__init__.py
@@ -5,4 +5,5 @@ from dlgrad import Tensor
 class Linear:
     def __init__(self, in_features: int, out_features: int) -> None:
         bound = 1 / math.sqrt(in_features)
-        self.weight = Tensor.
+        self.weight = Tensor.uniform((out_features, in_features), low=-bound, high=bound)
+        
