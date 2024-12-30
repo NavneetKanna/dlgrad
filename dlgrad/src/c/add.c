a45a59629cc2cde9df651f16bbfeaefe9158737f
NavneetKanna <navneetkanna04@gmail.com>
2024-12-30 07:09:16 +0530

    added add2d

--- dlgrad/src/c/add.c
+++ dlgrad/src/c/add.c
@@ -25,6 +25,8 @@ float *add_2d_dim1(float *x, float *y, int xnumel, int ncols)
         out[i] = x[i] + y[y_idx];
         y_idx += 1;
     }
+
+    return out;
 }
 
 float *add_2d_dim2(float *x, float *y, int xnumel, int ncols)
@@ -39,5 +41,15 @@ float *add_2d_dim2(float *x, float *y, int xnumel, int ncols)
         out[i] = x[i] + y[y_idx];
     }
 
+    return out;
+}
+
+float *add_2d(float *x, float *y, int xnumel)
+{
+    float *out = malloc(sizeof(float) * xnumel);
+
+    for (int i=0; i<xnumel; i++)
+        out[i] = x[i] + y[i];
+    
     return out;
 }
