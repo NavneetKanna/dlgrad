0a2e0a617799eaf0841ca3321846389bbc05acd5
NavneetKanna <navneetkanna04@gmail.com>
2025-01-12 16:27:08 +0530

    added calloc

--- dlgrad/src/c/allocate.c
+++ dlgrad/src/c/allocate.c
@@ -1,12 +1,28 @@
 #include <stdlib.h>
 #include "allocate.h"
 
-float *memory(size_t nbytes)
+
+float *uninitialized_memory(size_t num)
 {
-    float *out = malloc(nbytes*sizeof(float));
+    float *out = malloc(num*sizeof(float));
     if (out == NULL) {
         return NULL;
     }
 
     return out;
+}
+
+float *initialized_memory(size_t num)
+{
+    float *out = calloc(num, sizeof(float));
+    if (out == NULL) {
+        return NULL;
+    }
+
+    return out;
+}
+
+void free(float *ptr)
+{
+    free(ptr);
 }
