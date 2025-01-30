lools like i have fixed all gradient bugs, and now should be in a more stable state

dlgrad/ops.py
@@ -1,5 +1,5 @@
 from dlgrad.buffer import Buffer
-from dlgrad.helpers import CustomOps, check_broadcast
+from dlgrad.helpers import CustomOps, check_broadcast, find_broadcast_dim
 from dlgrad.tensor import OP
 
 # ------------ Unary Ops -----------
@@ -15,7 +15,11 @@ class Sum(OP):
 		return x.sum(dim=dim)
 
 	def backward(self, upstream_grad: Buffer) -> tuple[Buffer]:
-		return (Buffer.full(shape=self.inp_shape, fill_value=1.0, device=self.device),)
+		# print("-----")
+		t = Buffer.full(shape=self.inp_shape, fill_value=1.0, device=self.device)
+		# print("returning", t.shape)
+		# print("-----")
+		return (t*upstream_grad,)
 
 
 class Max(OP):
@@ -27,7 +31,7 @@ class Max(OP):
 		return self.out
 
 	def backward(self, upstream_grad: Buffer) -> tuple[Buffer]:
-		return (self.max_with_1s,)
+		return (self.max_with_1s*upstream_grad,)
 
 
 class Exp(OP):
@@ -42,11 +46,11 @@ class Exp(OP):
 class Log(OP):
 	def forward(self, x: Buffer) -> Buffer:
 		self.x = x
-		self.out = x.log()
-		return self.out
+		return x.log()
 
 	def backward(self, upstream_grad: Buffer) -> Buffer:
-		return upstream_grad / self.x
+		print("log backward called", upstream_grad.shape)
+		return (upstream_grad / self.x,)
 
 
 class Relu(OP):
@@ -68,8 +72,9 @@ class Add(OP):
 			return x + y
 
 	def backward(self, upstream_grad: Buffer) -> tuple[Buffer | None, Buffer | None]:
-		return self.match_inp_shape(inp=self.x, upstream_grad=upstream_grad) if self.req_grad[0] else None, \
-		  	   self.match_inp_shape(inp=self.y, upstream_grad=upstream_grad) if self.req_grad[1] else None
+		print("add backward", find_broadcast_dim(self.x.shape, self.y.shape))
+		return self.match_inp_shape(inp=self.x, upstream_grad=upstream_grad, dim=find_broadcast_dim(self.x.shape, self.y.shape)) if self.req_grad[0] else None, \
+		  	   self.match_inp_shape(inp=self.y, upstream_grad=upstream_grad, dim=find_broadcast_dim(self.x.shape, self.y.shape)) if self.req_grad[1] else None  # noqa: E501
 
 
 class Sub(OP):
@@ -80,8 +85,8 @@ class Sub(OP):
 			return x - y
 
 	def backward(self, upstream_grad: Buffer) -> tuple[Buffer | None, Buffer | None]:
-		return self.match_inp_shape(inp=self.x, upstream_grad=upstream_grad) if self.req_grad[0] else None, \
-		  	   self.match_inp_shape(inp=self.y, upstream_grad=-upstream_grad) if self.req_grad[1] else None
+		return self.match_inp_shape(inp=self.x, upstream_grad=upstream_grad, dim=find_broadcast_dim(self.x.shape, self.y.shape)) if self.req_grad[0] else None, \
+		  	   self.match_inp_shape(inp=self.y, upstream_grad=-upstream_grad, dim=find_broadcast_dim(self.x.shape, self.y.shape)) if self.req_grad[1] else None  # noqa: E501
 
 
 class Mul(OP):
@@ -92,8 +97,8 @@ class Mul(OP):
 			return x*y
 
 	def backward(self, upstream_grad: Buffer) -> Buffer:
-		return self.match_inp_shape(inp=self.x, upstream_grad=upstream_grad*self.y) if self.req_grad[0] else None, \
-		  	   self.match_inp_shape(inp=self.y, upstream_grad=upstream_grad*self.x) if self.req_grad[1] else None
+		return self.match_inp_shape(inp=self.x, upstream_grad=upstream_grad*self.y, dim=find_broadcast_dim(self.x.shape, self.y.shape)) if self.req_grad[0] else None, \
+		  	   self.match_inp_shape(inp=self.y, upstream_grad=upstream_grad*self.x, dim=find_broadcast_dim(self.x.shape, self.y.shape)) if self.req_grad[1] else None  # noqa: E501
 
 class Div(OP):
 	def forward(self, x: Buffer, y: Buffer) -> Buffer:

dlgrad/tensor.py
@@ -32,14 +32,14 @@ class OP:
 	def backward(self, *args, **kwargs) -> None:
 		raise RuntimeError(f"backward not implemented for {type(self)}")  # noqa: ANN201
 
-	def match_inp_shape(self, inp: Buffer, upstream_grad: Buffer) -> Buffer:
+	def match_inp_shape(self, inp: Buffer, upstream_grad: Buffer, dim: int = 0) -> Buffer:
 		inp_shape = inp.shape
 		ndim = resolve_ndim(inp_shape=inp_shape, grad_shape=upstream_grad.shape)
 		if not ndim:
 			return upstream_grad
 
 		for _ in range(ndim):
-			upstream_grad = upstream_grad.sum(dim=0)
+			upstream_grad = upstream_grad.sum(dim=dim)
 
 		if upstream_grad.shape != inp_shape:
 			upstream_grad.metadata.shape = inp_shape
@@ -82,6 +82,7 @@ import dlgrad.ops as ops  # since ops module imports OP class, it is placed afte
 # TODO: I am setting device here as well as in Buffer, fix this
 # TODO: Check dim out of bounds
 # TODO: Check 0d and 1d when creating tensor from np
+# TODO: What is max() dim ? does subtracting this with tensor work ?
 class Tensor:
 	def __init__(self, data: Buffer | "np.ndarray",  # type: ignore  # noqa: F821
 				 device: str | Device | None = Device.CPU, dtype: str | DType | None = None,
@@ -218,6 +219,7 @@ class Tensor:
 	@staticmethod
 	def sub(x: Tensor, y: Tensor) -> Tensor:
 		return ops.Sub.execute(x, y)
+		# return ops.Add.execute(x, -y)
 
 	@staticmethod
 	def div(x: Tensor, y: Tensor) -> Tensor:
@@ -260,10 +262,16 @@ class Tensor:
 		return ops.Log.execute(self)
 
 	def log_softmax(self) -> Tensor:
-		return ops.LogSoftmax.execute(self)
+		t = self.max(dim=1)
+		m = self - t
+		e = m.exp()
+		ss = e.sum(dim=1)
+		self.log_softmax_output = m - ss.log()
+		return self.log_softmax_output
+		# return ops.LogSoftmax.execute(self)
 
 	# TODO: If target shape does not match raise error
-	def cross_entropy_loss(self, target: Tensor) -> Tensor:  # noqa: ANN001
+	def cross_entropy_loss(self, target: Tensor) -> Tensor:
 		return ops.CrossEntropy.execute(self, target)
 
 	def backward(self) -> None:
@@ -291,14 +299,31 @@ class Tensor:
 			if node.grad is None:
 				raise RuntimeError(f"Tensor {node} has no grad")
 
+			print("-----")
+			print(node.grad.numpy())
 			upstream_grads: tuple[Buffer] = node._ctx.backward(node.grad.data)
 			upstream_grads: list[Tensor] = [
 				Tensor(g, device=self.device, requires_grad=False) for g in upstream_grads
 			]
+			i = 0
 			for p, g in zip(node._ctx.parents, upstream_grads):
+				i += 1
 				if p.requires_grad:
 					assert (g.shape == p.shape), f"Tensor shape and grad shape must match {p.shape}, {g.shape}"  # noqa: E501
-					p.grad = g if p.grad is None else p.grad + g
+					if p.grad is None:
+						print("p.grad is none")
+						p.grad = g
+						print(p.grad.numpy())
+					else:
+						print("p.grad is not none")
+						print("p.grad")
+						print(p.grad.numpy())
+						print("grad")
+						print(g.numpy())
+						p.grad = p.grad + g
+					# p.grad = g if p.grad is None else p.grad + g
+			# if i == 2:
+			# 	exit()
 
 	def __repr__(self) -> str:
 		return f"Tensor<dtype: {self.dtype} device: {self.device}, shape: {self.shape}, ndim: {self.ndim}>"  # noqa: E501
@@ -326,7 +351,7 @@ class Tensor:
 		return Tensor(data=self.data**val, device=self.device)
 
 	def __neg__(self) -> Tensor:
-		return Tensor(data=-self.data, device=self.device, dtype=self.dtype)
+		return Tensor(data=-self.data, device=self.device, dtype=self.dtype, requires_grad=self.requires_grad)
 
 	def __matmul__(self, other: Tensor) -> Tensor:
 		return Tensor.matmul(self, other)