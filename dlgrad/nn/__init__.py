00377b4e6f67cff9b49b419db872bd45059d4896
NavneetKanna <navneetkanna04@gmail.com>
2025-02-01 15:41:50 +0530

    added SGD, really exicited since i am close

--- dlgrad/nn/__init__.py
+++ dlgrad/nn/__init__.py
@@ -7,8 +7,8 @@ from dlgrad.nn import optim, utils  # noqa: F401
 class Linear:
     def __init__(self, in_features: int, out_features: int, bias: bool = True) -> None:
         bound = 1 / math.sqrt(in_features)
-        self.weight = Tensor.uniform((out_features, in_features), low=-bound, high=bound)
-        self.bias = Tensor.uniform((1, out_features), low=-bound, high=bound) if bias else None
+        self.weight = Tensor.uniform((out_features, in_features), low=-bound, high=bound, requires_grad=True)
+        self.bias = Tensor.uniform((1, out_features), low=-bound, high=bound, requires_grad=True) if bias else None
 
     def __call__(self, x: Tensor) -> Tensor:
         return x.linear(self.weight, self.bias)
--- dlgrad/nn/optim.py
+++ dlgrad/nn/optim.py
@@ -1,3 +1,23 @@
-class SGD:
-    def __init__(self):  # noqa: ANN204
-        pass
+from dlgrad.tensor import Tensor
+
+
+class Optimiser:
+    def __init__(self, params: list[Tensor], lr: int = 1e-3) -> None:
+        self.params = params
+        self.lr = lr
+
+    def step(self) -> None:
+        for i in self.params:
+            print(i)
+            print(i.grad)
+            assert i.shape == i.grad.shape, f"The shape of the weight {i.shape} and its grad {i.grad.shape} do not match"
+            i = i - self.lr*i.grad
+
+    def zero_grad(self) -> None:
+        for i in self.params:
+            i.grad = None
+
+
+class SDG(Optimiser):
+    def __init__(self, params: list[Tensor], lr: int = 1e-3) -> None:
+        super().__init__(params, lr)
