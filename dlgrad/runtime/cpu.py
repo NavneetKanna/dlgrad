faf142e340e06533f4fcb6fd3b0da46d5f0797ea
NavneetKanna <navneetkanna04@gmail.com>
2025-11-23 16:43:06 +0530

    changed pos of extra flasgs

--- dlgrad/runtime/cpu.py
+++ dlgrad/runtime/cpu.py
@@ -37,8 +37,6 @@ else:
 EXTRA_LIBS = []
 if sys.platform == "linux":
     EXTRA_LIBS = ["-lm", "-lmvec"]
-elif sys.platform == "darwin":
-    EXTRA_LIBS = ["-lm"]
 
 class CPU:
     """
@@ -53,7 +51,7 @@ class CPU:
         c_path = so_path.with_suffix(".c")
         c_path.write_text(source)
 
-        cmd = [COMPILER, *CFLAGS, *EXTRA_LIBS, "-o", str(so_path), str(c_path)]
+        cmd = [COMPILER, *CFLAGS, "-o", str(so_path), *EXTRA_LIBS, str(c_path)]
         res = subprocess.run(cmd, capture_output=True, text=True)
         if res.returncode != 0:
             raise RuntimeError(f"Compilation failed:\n{res.stderr}")
